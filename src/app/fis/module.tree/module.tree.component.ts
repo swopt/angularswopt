156019c370161b77470ad5bf652e10a46f20a5d0
wymeric89@gmail.com <wymeric89@gmail.com>
2017-03-02 23:11:51 +0800

    new commit

--- src/app/fis/module.tree/module.tree.component.ts
+++ src/app/fis/module.tree/module.tree.component.ts
@@ -5,12 +5,39 @@ import {ModuleTreeItem} from './module.tree';
 @Component({
     selector: 'module-tree',
     templateUrl: 'module.tree.html',
+    styleUrls: ['module.tree.css'],
     moduleId: module.id.replace('/dist/','src'),
 })
 
 export class ModuleTreeComponent {
     module: Array<ModuleTreeItem> = [
         {prgName:'Account Payable',subs: [
+            {prgName:'Enquiry',subs: [
+                {prgName:'Account Ledger Enquiry',subs:undefined},
+                {prgName:'Payment Voucher Enquiry',subs:undefined}
+            ]},
+            {prgName:'Maintenance',subs: [
+                {prgName:'Account Category Maintenance',subs:undefined},
+                {prgName:'AP Distribution Setting...',subs:undefined},
+                {prgName:'Batch Posting Utility',subs:undefined},
+                {prgName:'Creditor (Company)',subs:undefined},
+                {prgName:'Creditor (Individual)',subs:undefined}
+            ]}
+        ]},
+        {prgName:'Account Receivable',subs: [
+            {prgName:'Enquiry',subs: [
+                {prgName:'Account Ledger Enquiry',subs:undefined},
+                {prgName:'Payment Voucher Enquiry',subs:undefined}
+            ]},
+            {prgName:'Maintenance',subs: [
+                {prgName:'Account Category Maintenance',subs:undefined},
+                {prgName:'AP Distribution Setting...',subs:undefined},
+                {prgName:'Batch Posting Utility',subs:undefined},
+                {prgName:'Creditor (Company)',subs:undefined},
+                {prgName:'Creditor (Individual)',subs:undefined}
+            ]}
+        ]},
+        {prgName:'Cash Management',subs: [
             {prgName:'Enquiry',subs: [
                 {prgName:'Account Ledger Enquiry',subs:undefined},
                 {prgName:'Payment Voucher Enquiry',subs:undefined}
